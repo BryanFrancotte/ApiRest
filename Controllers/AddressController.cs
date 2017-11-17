6df7f4121d4b11a0cc36418af223d4b51b78ddb9
Bryan <francotte.bryan@gmail.com>
2017-11-17 09:42:30 +0100

    added some main methods

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -18,9 +18,24 @@ namespace ApiRest.Controllers
             }
         }
 
-        // GET api/Address/GetAllByUser/{userId}
-        [HttpGet("GetAllByUser/{userId}")]
-        public IActionResult GetAllByUser(int userId){
+        // GET api/Address/GetAllPickUpByUser/{userId}
+        [HttpGet("GetAllPickUpByUser/userId")]
+        public IActionResult GetAllPickUpAddressByUser(int userId){
+            using(var context = new _1718_etu32607_DBContext()){
+                if(context.User.Any(u => u.UserId == userId)){
+                    var listAddress = context.Address.Include(a => a.LocalityIdAddressNavigation)
+                                                    .Where(a => a.AddressId == context.Order.Where(o => o.UserIdOrder == userId)
+                                                                                            .Single().PickUpAddress)
+                                                    .ToList();
+                    return Ok(listAddress);
+                }
+                return NotFound();
+            }
+        }
+
+        // GET api/Address/GetAllDepositByUser/{userId}
+        [HttpGet("GetAllDepositByUser/{userId}")]
+        public IActionResult GetAllDepositAddressByUser(int userId){
             using(var context = new _1718_etu32607_DBContext()){
                 if(context.User.Any(u => u.UserId == userId)){
                     var listAddress = context.Address.Include(a => a.LocalityIdAddressNavigation)
@@ -30,6 +45,19 @@ namespace ApiRest.Controllers
                     return Ok(listAddress);
                 }
                 return NotFound();
+            }
+        }
+
+        // PUT api/Address/Add
+        [HttpPut("Add")]
+        public IActionResult AddAddress([FromBody]Address address){
+            using(var context = new _1718_etu32607_DBContext()){
+                if(ModelState.IsValid){
+                    context.Address.Add(address);
+                    context.SaveChanges();
+                    return Ok();
+                }
+                return BadRequest();
             }
         }
     }
